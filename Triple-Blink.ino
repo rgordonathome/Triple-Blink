051ade15696311df5364e9d3a54ff692f666feb9
Russell Gordon <rgordon@rsgc.on.ca>
2015-11-13 15:35:12 -0500

    LEDs now rotate, stoplight fashion.

--- Triple-Blink.ino
+++ Triple-Blink.ino
@@ -9,24 +9,34 @@ void setup() {
   pinMode(yellowLED, OUTPUT);
   pinMode(redLED, OUTPUT);
 
-}
-
-void loop() {
-  
-  // turn on all the LEDs
-  digitalWrite(greenLED, HIGH);
-  digitalWrite(yellowLED, HIGH);
-  digitalWrite(redLED, HIGH);
-
-  // wait 1 second
-  delay(1000);
-
   // turn off all LEDs
   digitalWrite(greenLED, LOW);
   digitalWrite(yellowLED, LOW);
   digitalWrite(redLED, LOW);
 
+}
+
+void loop() {
+  
+  // turn on green LED
+  digitalWrite(greenLED, HIGH);
   // wait 1 second
   delay(1000);
+  // turn off green LED
+  digitalWrite(greenLED, LOW);
+  
+  // turn on yellow LED
+  digitalWrite(yellowLED, HIGH);
+  // wait 1 second
+  delay(1000);
+  // turn off yellow LED
+  digitalWrite(yellowLED, LOW);
+
+  // turn on green LED
+  digitalWrite(redLED, HIGH);
+  // wait 1 second
+  delay(1000);
+  // turn off green LED
+  digitalWrite(redLED, LOW);
 
 }
